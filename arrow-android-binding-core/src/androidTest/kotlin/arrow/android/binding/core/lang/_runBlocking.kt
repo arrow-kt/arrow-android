a05d527f311a36849c1bc4795cf106bef881ddef
Pablisco <pabs87@gmail.com>
2020-09-29 00:50:32 +0100

    formatting and scoping of functions

--- arrow-android-binding-core/src/androidTest/kotlin/arrow/android/binding/core/lang/_runBlocking.kt
+++ arrow-android-binding-core/src/androidTest/kotlin/arrow/android/binding/core/lang/_runBlocking.kt
@@ -1,4 +1,4 @@
 package arrow.android.binding.core.lang
 
-fun <A, R> A.runBlocking(block: suspend A.() -> R): R =
+internal fun <A, R> A.runBlocking(block: suspend A.() -> R): R =
     kotlinx.coroutines.runBlocking { block() }
