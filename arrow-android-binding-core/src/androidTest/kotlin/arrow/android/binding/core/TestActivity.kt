03fd943a071c5e6409f82f28d534128cbdc0a666
Pablisco <pabs87@gmail.com>
2020-09-29 00:48:07 +0100

    Updated tests with prototype for testing streams

--- arrow-android-binding-core/src/androidTest/kotlin/arrow/android/binding/core/TestActivity.kt
+++ arrow-android-binding-core/src/androidTest/kotlin/arrow/android/binding/core/TestActivity.kt
@@ -1,5 +1,20 @@
 package arrow.android.binding.core
 
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.test.core.app.ActivityScenario
 
 class TestActivity : AppCompatActivity()
+
+suspend fun ActivityScenario<TestActivity>.withLayout(
+    @LayoutRes layout: Int,
+    whenCreated: suspend TestActivity.() -> Unit,
+) {
+    onActivity { activity ->
+        activity.setContentView(layout)
+        activity.lifecycleScope.launchWhenCreated {
+            activity.whenCreated()
+        }
+    }
+}
--- arrow-android-binding-core/src/androidTest/kotlin/arrow/android/binding/core/ViewExtensionTest.kt
+++ arrow-android-binding-core/src/androidTest/kotlin/arrow/android/binding/core/ViewExtensionTest.kt
@@ -1,19 +1,12 @@
 package arrow.android.binding.core
 
 import android.widget.Button
-import androidx.lifecycle.lifecycleScope
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.longClick
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.activityScenarioRule
 import arrow.android.binding.core.test.R
-import arrow.fx.coroutines.stream.Stream
-import arrow.fx.coroutines.stream.concurrent.Queue
-import arrow.fx.coroutines.stream.drain
-import arrow.fx.coroutines.stream.toList
-import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
 
@@ -22,43 +15,28 @@ class ViewExtensionTest {
     @get:Rule
     val activityRule = activityScenarioRule<TestActivity>()
 
-    @Test
-    fun clickEmitsUnitIntoStream(): Unit = runBlocking {
-        val events = activityRule.scenario.captureButtonEvents { clicks() }
-
-        onView(withId(R.id.button)).perform(click())
-        onView(withId(R.id.button)).perform(click())
-
-        check(events.take(2).toList() == listOf(Unit, Unit))
-    }
 
     @Test
-    fun longClickEmitsUnitIntoStream(): Unit = runBlocking {
-        val events = activityRule.scenario.captureButtonEvents { longClicks() }
-
-        onView(withId(R.id.button)).perform(longClick())
-        onView(withId(R.id.button)).perform(longClick())
-
-        check(events.take(2).toList() == listOf(Unit, Unit))
-    }
-
-}
-
-private suspend fun <T> ActivityScenario<TestActivity>.captureButtonEvents(
-    findEvents: Button.() -> Stream<T>
-): Stream<T> = captureStream {
-    onActivity { activity ->
-        activity.setContentView(R.layout.test_activity_button)
-        val button = activity.findViewById<Button>(R.id.button)
-        activity.lifecycleScope.launchWhenCreated {
-            button.findEvents().capture()
+    fun clickEmitsUnitIntoStream(): Unit = testStream {
+        activityRule.scenario.withLayout(R.layout.test_activity_button) {
+            findViewById<Button>(R.id.button).clicks().capture()
         }
+        onView(withId(R.id.button)).perform(click())
+        expect(Unit)
+        onView(withId(R.id.button)).perform(click())
+        expect(Unit)
+        expectNothingMore()
     }
-}
 
-suspend fun <O> captureStream(block: suspend StreamCaptor<O>.() -> Unit): Stream<O> =
-    StreamCaptor<O>().apply { block() }.queue.dequeue()
-
-class StreamCaptor<O>(val queue: Queue<O> = Queue.unsafeUnbounded()) {
-    suspend fun Stream<O>.capture(): Unit = effectTap { queue.enqueue1(it) }.drain()
+    @Test
+    fun longClickEmitsUnitIntoStream(): Unit = testStream {
+        activityRule.scenario.withLayout(R.layout.test_activity_button) {
+            findViewById<Button>(R.id.button).longClicks().capture()
+        }
+        onView(withId(R.id.button)).perform(longClick())
+        expect(Unit)
+        onView(withId(R.id.button)).perform(longClick())
+        expect(Unit)
+        expectNothingMore()
+    }
 }
